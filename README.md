1da72d421df77f2d8eefccb1bd1c4424800034f2
Afevis <ShizCalev@users.noreply.github.com>
2025-05-02 06:56:19 -0600

    initial release

---
+++ README.md
@@ -0,0 +1,25 @@
+# Metal Gear Solid 2 - 2003 PC / GOG Version Skybox Fix
+
+
+- Restores all the skybox models broken in the 2003 PC / Substance version of MGS2 using the fix made by cipherxof / triggerhappy for MGSHDFix.
+
+## Installation
+
+- Make sure [V's Fix](https://github.com/VFansss/mgs2-v-s-fix) is installed!
+- Grab the latest release of MGS2-2003-SkyboxFix from [here.](https://github.com/Afevis/MGS2-2003-SkyboxFix/releases)
+- Extract the contents of the release zip into the game's scripts folder.<br />(e.g. "**Metal Gear Solid 2 Substance\bin\scripts**" for Steam).
+
+  
+## Screenshots
+
+
+|:--:|
+| Metal Gear Solid 2 |
+
+
+## Credits
+[@Lyall](https://github.com/lyall) for MGSHDFix framework. <br />
+[Ultimate ASI Loader](https://github.com/ThirteenAG/Ultimate-ASI-Loader) for ASI loading. <br />
+[inipp](https://github.com/mcmtroffaes/inipp) for ini reading. <br />
+[spdlog](https://github.com/gabime/spdlog) for logging. <br />
+[safetyhook](https://github.com/cursey/safetyhook) for hooking.
---
+++ src/dllmain.cpp
@@ -0,0 +1,106 @@
+#include "stdafx.h"
+#include "helper.hpp"
+#include <string>
+#include <string_view>
+
+#include <inipp/inipp.h>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/base_sink.h>
+#include <safetyhook.hpp>
+
+HMODULE baseModule = GetModuleHandle(NULL);
+std::filesystem::path sExePath;
+std::string sExeName;
+
+void Main()
+{ 
+    uintptr_t MGS2_CreateSkyUtilScanResult = (uintptr_t)Memory::PatternScan(baseModule, "81 CA 00 30 00 00 85 C0 89 56");
+    Memory::PatchBytes(MGS2_CreateSkyUtilScanResult, "\x90\x90\x90\x90\x90\x90", 6);
+
+}
+
+
+std::mutex mainThreadFinishedMutex;
+std::condition_variable mainThreadFinishedVar;
+bool mainThreadFinished = false;
+
+DWORD __stdcall Main(void*)
+{
+    WCHAR exePath[_MAX_PATH] = { 0 };
+    GetModuleFileNameW(baseModule, exePath, MAX_PATH);
+    sExePath = exePath;
+    sExeName = sExePath.filename().string();
+    sExePath = sExePath.remove_filename();
+    if (sExeName == "mgs2_sse.exe")
+    {
+        Main();
+    }
+
+    // Signal any threads which might be waiting for us before continuing
+    {
+        std::lock_guard lock(mainThreadFinishedMutex);
+        mainThreadFinished = true;
+        mainThreadFinishedVar.notify_all();
+    }
+
+    return true;
+}
+
+std::mutex memsetHookMutex;
+bool memsetHookCalled = false;
+void* (__cdecl* memset_Fn)(void* Dst, int Val, size_t Size);
+void* __cdecl memset_Hook(void* Dst, int Val, size_t Size)
+{
+    // memset is one of the first imports called by game (not the very first though, since ASI loader still has those hooked during our DllMain...)
+    std::lock_guard lock(memsetHookMutex);
+    if (!memsetHookCalled)
+    {
+        memsetHookCalled = true;
+
+        // First we'll unhook the IAT for this function as early as we can
+        Memory::HookIAT(baseModule, "VCRUNTIME140.dll", memset_Hook, memset_Fn);
+
+        // Wait for our main thread to finish before we return to the game
+        if (!mainThreadFinished)
+        {
+            std::unique_lock finishedLock(mainThreadFinishedMutex);
+            mainThreadFinishedVar.wait(finishedLock, [] { return mainThreadFinished; });
+        }
+    }
+
+    return memset_Fn(Dst, Val, Size);
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    {
+        // Try hooking IAT of one of the imports game calls early on, so we can make it wait for our Main thread to complete before returning back to game
+        // This will only hook the main game modules usage of memset, other modules calling it won't be affected
+        HMODULE vcruntime140 = GetModuleHandleA("VCRUNTIME140.dll");
+        if (vcruntime140)
+        {
+            memset_Fn = decltype(memset_Fn)(GetProcAddress(vcruntime140, "memset"));
+            Memory::HookIAT(baseModule, "VCRUNTIME140.dll", memset_Fn, memset_Hook);
+        }
+
+        HANDLE mainHandle = CreateThread(NULL, 0, Main, 0, NULL, 0);
+        if (mainHandle)
+        {
+            SetThreadPriority(mainHandle, THREAD_PRIORITY_HIGHEST); // set our Main thread priority higher than the games thread
+            CloseHandle(mainHandle);
+        }
+    }
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
